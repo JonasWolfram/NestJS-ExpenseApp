bb9abe726e221f3d27f6b39aba85d8c131a35899
Jonas Wolfram <jonas.wolfram@me.com>
2022-07-12 06:26:48 +0200

    added summary service

--- src/summary/summary.controller.ts
+++ src/summary/summary.controller.ts
@@ -3,10 +3,11 @@ import { SummaryService } from './summary.service';
 
 @Controller('summary')
 export class SummaryController {
-  constructor(private readonly summaryService: SummaryService);
+  constructor(private readonly summaryService: SummaryService) {}
 
   @Get()
   getSummary() {
+    // Aufruf des Services im Controller
     return this.summaryService.calculateSummary();
   }
 }
